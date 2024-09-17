9466ff5bd06d645f6bca1fd0c80350113f07972e
root <andygyakobo@gmail.com>
2024-09-17 12:25:28 -0400

    Implementing the EmailAdapter

--- lab2/app/src/main/java/com/example/lab2/EmailAdapter.kt
+++ lab2/app/src/main/java/com/example/lab2/EmailAdapter.kt
@@ -1,5 +1,9 @@
 package com.example.lab2
 
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
 class EmailAdapter {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -8,9 +12,17 @@ class EmailAdapter {
 
         // We also create a constructor that accepts the entire item row
         // and does the view lookups to find each sub-view
+
+        val senderTextView: TextView
+        val titleTextView: TextView
+        val summaryTextView: TextView
+
         init {
             // TODO: Store each of the layout's views into
             // the public final member variables created above
+            senderTextView = itemView.findViewById(R.id.senderTv)
+            titleTextView = itemView.findViewById(R.id.titleTv)
+            summaryTextView = itemView.findViewById(R.id.summaryTv)
         }
     }
 }
