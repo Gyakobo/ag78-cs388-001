942773e5077255d5d82f4c7516d6cfd29c62da8b
root <andygyakobo@gmail.com>
2024-09-17 12:21:12 -0400

    Made the EmailAdapter class

---
+++ lab2/app/src/main/java/com/example/lab2/EmailAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.lab2
+
+class EmailAdapter {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        // TODO: Create member variables for any view that will be set
+        // as you render a row.
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each sub-view
+        init {
+            // TODO: Store each of the layout's views into
+            // the public final member variables created above
+        }
+    }
+}
