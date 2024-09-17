c0caa81c403eb6336d679302aa45eb9a333856a8
root <andygyakobo@gmail.com>
2024-09-17 12:11:39 -0400

    Started doing lab2

---
+++ lab2/app/src/main/java/com/example/lab2/Email.kt
@@ -0,0 +1,9 @@
+package com.example.lab2
+
+class Email(
+    val sender: String,
+    val title: String,
+    val summary: String,
+) {
+
+}
---
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.lab2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    class Email (
+        val sender: String,
+        val title: String,
+        val summary: String
+    ) {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
