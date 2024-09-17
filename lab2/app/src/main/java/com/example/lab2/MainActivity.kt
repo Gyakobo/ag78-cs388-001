58982d855406549ad618bb5226d819c8567961f4
root <andygyakobo@gmail.com>
2024-09-17 12:15:56 -0400

    Created the item layout.xml

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -8,14 +8,6 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    class Email (
-        val sender: String,
-        val title: String,
-        val summary: String
-    ) {
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
