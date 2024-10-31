a064e9bb2a62357ff2afa804545bb7994cdeb58a
Gyakobo <andygyakobo@gmail.com>
2024-10-31 19:53:53 -0400

    The Gradle issue seems to be fixed, plugging in the API-KEY

--- lab5/app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
+++ lab5/app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
@@ -13,7 +13,7 @@ import com.bumptech.glide.Glide
 const val ARTICLE_EXTRA = "ARTICLE_EXTRA"
 private const val TAG = "ArticleAdapter"
 
-class ArticleAdapter(private val context: Context, private val articles: List<Article>) :
+class ArticleAdapter(private val context: Context, private val articles: List<DisplayArticle>) :
     RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,8 +22,8 @@ class ArticleAdapter(private val context: Context, private val articles: List<Ar
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val article = articles[position]
-        holder.bind(article)
+        val displayArticle = articles[position]
+        holder.bind(displayArticle)
     }
 
     override fun getItemCount() = articles.size
@@ -39,8 +39,8 @@ class ArticleAdapter(private val context: Context, private val articles: List<Ar
             itemView.setOnClickListener(this)
         }
 
-        fun bind(article: Article) {
-            titleTextView.text = article.headline?.main
+        fun bind(article: DisplayArticle) {
+            titleTextView.text = article.headline.toString()
             abstractTextView.text = article.abstract
 
             Glide.with(context)
--- lab5/app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ lab5/app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -25,7 +28,7 @@ private const val ARTICLE_SEARCH_URL =
     "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
-    private val articles = mutableListOf<Article>()
+    private val articles = mutableListOf<DisplayArticle>()
     private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
@@ -63,31 +66,50 @@ class MainActivity : AppCompatActivity() {
                         json.jsonObject.toString()
                     )
                     parsedJson.response?.docs?.let { list ->
-                        lifecycleScope.launch {
-                            (application as ArticleApplication).db.articleDao().getAll().collect { databaseList ->
-                                databaseList.map { entity ->
-                                    DisplayArticle(
-                                        entity.headline,
-                                        entity.articleAbstract,
-                                        entity.byline,
-                                        entity.mediaImageUrl
-                                    )
-                                }.also { mappedList ->
-                                    articles.clear()
-                                    articles.addAll(mappedList)
-                                    articleAdapter.notifyDataSetChanged()
-                                }
-                            }
+                        lifecycleScope.launch(Dispatchers.IO) {
+                            // Clear existing data in the database
+                            (application as ArticleApplication).db.articleDao().deleteAll()
+
+                            // Insert new data into the database
+                            (application as ArticleApplication).db.articleDao().insertAll(list.map {
+                                ArticleEntity(
+                                    headline = it.headline?.main,
+                                    articleAbstract = it.abstract,
+                                    byline = it.byline?.original,
+                                    mediaImageUrl = it.mediaImageUrl
+                                )
+                            })
                         }
-                    })
 
                     }
+
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
                 }
             }
 
         })
+    }
 
+    private fun loadArticlesFromDatabase() {
+        // We want this coroutine to be in the Main dispatcher as it will update the UI.
+        lifecycleScope.launch {
+            (application as ArticleApplication).db.articleDao().getAll().collect { databaseList ->
+                // Map the ArticleEntities to DisplayArticles
+                val mappedList = databaseList.map { entity ->
+                    DisplayArticle(
+                        headline = entity.headline,
+                        abstract = entity.articleAbstract,
+                        byline = entity.byline,
+                        mediaImageUrl = entity.mediaImageUrl
+                    )
+                }
+
+                // Update the articles list and notify the adapter on the Main thread
+                articles.clear()
+                articles.addAll(mappedList)
+                binding.articles.adapter?.notifyDataSetChanged()
+            }
+        }
     }
 }
