21218fc9cd797a85d3152a33e2d89d0aaf72dc2f
Gyakobo <andygyakobo@gmail.com>
2024-10-31 21:55:26 -0400

    Fixed the error with the Gradle but the app is not displaying anything

--- lab5/app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
+++ lab5/app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
@@ -26,8 +26,8 @@ class DetailActivity : AppCompatActivity() {
         val article = intent.getSerializableExtra(ARTICLE_EXTRA) as DisplayArticle
 
         // Set title and abstract information for the article
-        titleTextView.text = article.headline?.main
-        bylineTextView.text = article.byline?.original
+        titleTextView.text = article.headline
+        bylineTextView.text = article.byline
         abstractTextView.text = article.abstract
 
         // Load the media image
