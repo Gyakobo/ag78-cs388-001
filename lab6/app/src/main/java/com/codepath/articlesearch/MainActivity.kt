3887a3e459c479e7140d5310118501575a84bcfa
Gyakobo <andygyakobo@gmail.com>
2024-11-25 07:28:55 -0500

    Small patch to README

--- lab6/app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ lab6/app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.codepath.articlesearch
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
-import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
-import okhttp3.Headers
-import org.json.JSONException
 
 fun createJson() = Json {
     isLenient = true
@@ -29,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
 
-    private fun replaceFragment(articleListFragment: ArticleListFragment) {
+    private fun replaceFragment(articleListFragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.article_frame_layout, articleListFragment)
